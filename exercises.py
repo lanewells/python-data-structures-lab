ef4b9dae20146b24064c2fde6adaadae02134ed4
lanewells <wellsdelaneyk@gmail.com>
2024-11-26 14:09:42 -0800

    Exercise 2 complete

--- exercises.py
+++ exercises.py
@@ -1,5 +1,5 @@
-# Exercise 1: List and Indexing
-#
+# Exercise 1: List and Indexing #####################
+
 # Create a list named students containing at least three student names (strings).
 # Assign the second student’s name to a variable named first_student.
 # Assign the last student’s name to a variable named last_student.
@@ -13,3 +13,20 @@ def manage_students():
 
 # Call the function and print the result
 print('Exercise 1:', manage_students())
+
+# Exercise 2: Loop and String Concatenation #####################
+#
+# Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
+# Create a variable named meal and assign an empty string to it.
+# Use a for loop to iterate over the strings in foods and append each string to meal.
+
+def combine_foods():
+    # your code here
+    foods = 'cheese', 'bread', 'kombucha', 'champagne'
+    meal = ''
+    for food in foods:
+        meal += food + ' '
+    return meal
+
+# Call the function and print the result
+print('Exercise 2:', combine_foods())
